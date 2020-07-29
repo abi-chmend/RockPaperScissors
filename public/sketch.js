47ab0266063dd349083b637ebcfaba0bd17d2d5a
wa1rus <whales4fun@hotmail.com>
2020-07-28 17:20:37 -0700

    added rps demo logic

--- public/sketch.js
+++ public/sketch.js
@@ -4,6 +4,9 @@
 
 // Keep track of our socket connection
 var socket;
+var players = []
+var move = ""
+var thisPlayer;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,42 +16,51 @@ function setup() {
   socket = io.connect('http://localhost:3000');
   // We make a named event called 'mouse' and write an
   // anonymous callback function
-  socket.on('mouse',
-    // When we receive data
-    function(data) {
-      console.log("Got: " + data.x + " " + data.y);
-      // Draw a blue circle
-      fill(0,0,255);
-      noStroke();
-      ellipse(data.x, data.y, 20, 20);
-    }
-  );
+
+  // socket.on('score',
+  //     function (score) {
+  //     background(0)
+  //         console.log(`Current score: ${score}`);
+  //         fill(255)
+  //         text(score, 100,100)
+  //         text(move, 100,150)
+  //     }
+  // )
+    socket.on('players',
+        function (players) {
+            players = players
+            for(let i =0;i<players.length;i++){
+                if(players[i].socketId == socket.id){
+                    thisPlayer = players[i]
+                    background(0)
+                    fill(255)
+                    text(thisPlayer.score, 100,100)
+                    text(thisPlayer.move, 100,150)
+                }
+            }
+        }
+        )
+
 }
 
 function draw() {
   // Nothing
 }
 
-function mouseDragged() {
-  // Draw some white circles
-  fill(255);
-  noStroke();
-  ellipse(mouseX,mouseY,20,20);
-  // Send the mouse coordinates
-  sendmouse(mouseX,mouseY);
+function mousePressed(){
+    var rand = Math.round(Math.random()*3+.5)
+    switch (rand) {
+        case 1:move = "rock"; break;
+        case 2:move = "paper"; break;
+        case 3:move = "scissors"; break;
+    }
+    var moveObj = {
+        move:move,
+        socketId:socket.id
+    }
+    text(move, 100,150)
+    socket.emit('move',moveObj)
 }
 
 // Function for sending to the socket
-function sendmouse(xpos, ypos) {
-  // We are sending!
-  console.log("sendmouse: " + xpos + " " + ypos);
-  
-  // Make a little object with  and y
-  var data = {
-    x: xpos,
-    y: ypos
-  };
 
-  // Send that object to the socket
-  socket.emit('mouse',data);
-}
--- server.js
+++ server.js
@@ -2,19 +2,27 @@
 // http://itp.nyu.edu/~sve204/liveweb_fall2013/week3.html
 
 // Using express: http://expressjs.com/
+var players = []
+var turnCount = 0;
 var express = require('express');
 // Create the app
 var app = express();
 
 // Set up the server
 // process.env.PORT is related to deploying on heroku
-var server = app.listen(3000);
+var server = require('http').Server(app);
+server.listen(3000, function () {
+	console.log(`Listening on ${server.address().port}`);
+});
 
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/public/index.html');
+});
 // This call back just tells us that the server has started
 function listen() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://' + host + ':' + port);
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Example app listening at http://' + host + ':' + port);
 }
 
 app.use(express.static('public'));
@@ -27,28 +35,74 @@ var io = require('socket.io')(server);
 // Register a callback function to run when we have an individual connection
 // This is run for each individual user that connects
 io.sockets.on('connection',
-  // We are given a websocket object in our function
-  function (socket) {
-  
-    console.log("We have a new client: " + socket.id);
-  
-    // When this user emits, client side: socket.emit('otherevent',some data);
-    socket.on('mouse',
-      function(data) {
-        // Data comes in as whatever was sent, including objects
-        console.log("Received: 'mouse' " + data.x + " " + data.y);
-      
-        // Send it to all other clients
-        socket.broadcast.emit('mouse', data);
-        
-        // This is a way to send to everyone including sender
-        io.sockets.emit('message', "this goes to everyone");
+	// We are given a websocket object in our function
+	function (socket) {
 
-      }
-    );
-    
-    socket.on('disconnect', function() {
-      console.log("Client has disconnected");
-    });
-  }
+
+		console.log("We have a new client: " + socket.id);
+		players.push(new player(socket.id))
+
+		// When this user emits, client side: socket.emit('otherevent',some data);
+		socket.on('mouse',
+			function(data) {
+				// Data comes in as whatever was sent, including objects
+				console.log("Received: 'mouse' " + data.x + " " + data.y);
+				// console.log(players.length)
+
+				// Send it to all other clients
+				socket.broadcast.emit('mouse', data);
+
+				// This is a way to send to everyone including sender
+				io.sockets.emit('message', "this goes to everyone");
+
+			}
+		);
+		socket.on('move',
+			function(moveObj){
+				console.log(moveObj.move)
+				for(let i =0;i<players.length;i++){
+					if(players[i].socketId == moveObj.socketId){
+						players[i].setMove(moveObj.move)
+						if(players[i].move == "rock"){players[i].incrementScore()}
+					}
+				}
+				io.sockets.emit('score', players[0].score)
+				io.sockets.emit('players' , players)
+			}
+		);
+
+
+		socket.on('disconnect', function() {
+			console.log("Client has disconnected");
+			for(let i =0;i<players.length;i++){
+				if(players[i].socketId == socket.id){
+					players.splice(i,1)
+				}
+			}
+		});
+	}
 );
+function gameLogic(p1, p2){
+	if(p1.move == p2.move){
+		return null;
+	}
+	else if(p1.move == "rock" && p2.move == "scissors" ||
+		p1.move == "scissors" && p2.move == "paper" ||
+		p1.move == "paper" && p2.move == "rock" ){
+		return p1
+	}
+	else{
+		return p2
+	}
+}
+class player{
+	constructor(socketId) {
+		this.socketId = socketId
+		this.name = ""
+		this.score = 0
+		this.move = ""
+	}
+	incrementScore(){this.score++;}
+	setName(name){this.name = name}
+	setMove(move){this.move = move}
+}
